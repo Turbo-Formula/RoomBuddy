e258bd59d56b2de78b3d62569a1a9c27e9439d0f
Rev Neil Wakely <neil@turboformula.co.uk>
2017-04-06 09:20:55 +0100

    First code completed
    Waiting for database's te be set up to test this code.

--- PHP Files/changeLog.php
+++ PHP Files/changeLog.php
@@ -1,13 +1,63 @@
 <?php
-	$revision = $rev."\n"; 
-	$logFileName = $rNum.$rev
-	$logFile = fopen($logFileName, "w") or die("Unable to open file!");
-	$lastUpdatedBy = slMember."\n";
-	$lastUpdatedTime = $rLast."\n";
-	$changesMade = $changes;
+	$type = $changeType;
+
+	if ($changeType = "booking")
+	{
+		$revision = $rev."\n"; 
+		$logFileName = $changeType.$rNum.$rev
+		$logFile = fopen($logFileName, "w") or die("Unable to open file!");
+		$lastUpdatedBy = slMember."\n";
+		$lastUpdatedTime = $rLast."\n";
+		$changesMade = $changes;
 	
-	fwrite($logFileName, $lastUpdatedBy);
-	fwrite($logFileName, $lastUpdatedTime);
-	fwrite($logFileName, $changesMade);
-	fclose($logFileName);
+		fwrite($logFileName, $lastUpdatedBy);
+		fwrite($logFileName, $lastUpdatedTime);
+		fwrite($logFileName, $changesMade);
+		fclose($logFileName);
+	}
+
+	if ($changeType = "customer")
+	{
+		$revision = $rev."\n"; 
+		$logFileName = $changeType.$name.$rev
+		$logFile = fopen($logFileName, "w") or die("Unable to open file!");
+		$lastUpdatedBy = slMember."\n";
+		$lastUpdatedTime = $rLast."\n";
+		$changesMade = $changes;
+	
+		fwrite($logFileName, $lastUpdatedBy);
+		fwrite($logFileName, $lastUpdatedTime);
+		fwrite($logFileName, $changesMade);
+		fclose($logFileName);
+	}
+	
+	if ($changeType = "catering")
+	{
+		$revision = $rev."\n"; 
+		$logFileName = $changeType.$name.$rev
+		$logFile = fopen($logFileName, "w") or die("Unable to open file!");
+		$lastUpdatedBy = slMember."\n";
+		$lastUpdatedTime = $rLast."\n";
+		$changesMade = $changes;
+	
+		fwrite($logFileName, $lastUpdatedBy);
+		fwrite($logFileName, $lastUpdatedTime);
+		fwrite($logFileName, $changesMade);
+		fclose($logFileName);
+	}
+	
+	if ($changeType = "assets")
+	{
+		$revision = $rev."\n"; 
+		$logFileName = $changeType.$name.$rev
+		$logFile = fopen($logFileName, "w") or die("Unable to open file!");
+		$lastUpdatedBy = slMember."\n";
+		$lastUpdatedTime = $rLast."\n";
+		$changesMade = $changes;
+	
+		fwrite($logFileName, $lastUpdatedBy);
+		fwrite($logFileName, $lastUpdatedTime);
+		fwrite($logFileName, $changesMade);
+		fclose($logFileName);
+	}
 ?> 
